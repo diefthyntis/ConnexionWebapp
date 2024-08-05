6f2b7ae720f3b92780ca358bb43b3993e8276cea
Alan Hauser <diefthyntis@gmail.com>
2024-08-05 15:10:00 +0200

    Premier commit avec projet STS vierge

---
+++ src/main/java/com/diefthyntis/ConnexionWebapp/ConnexionWebappApplication.java
@@ -0,0 +1,13 @@
+package com.diefthyntis.ConnexionWebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnexionWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnexionWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diefthyntis/ConnexionWebapp/ConnexionWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diefthyntis.ConnexionWebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnexionWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
